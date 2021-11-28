84c85d469264b66011c9120c6ba77d85c49161ef
Luciano <lcriscola@hotmail.com>
2021-11-28 15:03:21 -0500

    Commit on Deploy

--- AmcrestMQTT/publish amcrest 2 mqtt.ps1
+++ AmcrestMQTT/publish amcrest 2 mqtt.ps1
@@ -4,7 +4,8 @@ $config = ConvertFrom-Json  -InputObject $configContent
 $newVersion = [System.Convert]::ToInt32($config.version)+1
 $config.version = $newVersion 
 $configContent = convertto-json $config -Depth 10
-$configContent 
+$newVersion
+#$configContent 
 Set-Content config.json -Value $configContent 
 git commit * -m "Commit on Deploy"
 git push
