98064a4af9a8d4c0b030acc9a804ef5f68864d22
Luciano <lcriscola@hotmail.com>
2021-11-28 15:02:48 -0500

    a

---
+++ AmcrestMQTT/publish amcrest 2 mqtt.ps1
@@ -0,0 +1,10 @@
+﻿cd AmcrestMQTT
+$configContent = get-content config.json -Raw
+$config = ConvertFrom-Json  -InputObject $configContent
+$newVersion = [System.Convert]::ToInt32($config.version)+1
+$config.version = $newVersion 
+$configContent = convertto-json $config -Depth 10
+$configContent 
+Set-Content config.json -Value $configContent 
+git commit * -m "Commit on Deploy"
+git push
